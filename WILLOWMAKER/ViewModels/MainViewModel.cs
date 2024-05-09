310a99543f4cd6044114426d1599cf2f0cb0ebad
[K]ONGOR <project.kongor@proton.me>
2024-05-10 00:33:36 +0100

    Add Links

--- WILLOWMAKER/ViewModels/MainViewModel.cs
+++ WILLOWMAKER/ViewModels/MainViewModel.cs
@@ -2,5 +2,8 @@
 
 public partial class MainViewModel : ViewModelBase
 {
-    public string Greeting => "ლ(ಠ益ಠლ) But At What Cost?";
+    public string GitHubLink => "https://github.com/Project-KONGOR-Open-Source";
+    public string WebPortalLink => "https://kongor.online/";
+    public string RedditLink => "https://www.reddit.com/r/Project_KONGOR/";
+    public string DiscordLink => "ლ(ಠ益ಠლ) But At What Cost?";
 }
