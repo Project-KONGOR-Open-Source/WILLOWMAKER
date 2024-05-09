6615942477250a9b9f93850ba6c5ea2d2b19a748
[K]ONGOR <project.kongor@proton.me>
2024-05-09 20:00:22 +0100

    Add Initial Solution Files

---
+++ WILLOWMAKER/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace WILLOWMAKER
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ WILLOWMAKER/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace WILLOWMAKER
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
