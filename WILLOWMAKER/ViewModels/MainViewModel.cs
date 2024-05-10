3eb0f55711681747a264babbf517ba3a1151de27
[K]ONGOR <project.kongor@proton.me>
2024-05-10 23:34:01 +0100

    Add Dropdown For Selecting The Master Server Address

--- WILLOWMAKER/ViewModels/MainViewModel.cs
+++ WILLOWMAKER/ViewModels/MainViewModel.cs
@@ -1,9 +1,24 @@
-﻿namespace WILLOWMAKER.ViewModels;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
 
-public partial class MainViewModel : ViewModelBase
+namespace WILLOWMAKER.ViewModels;
+
+// This class needs to be partial, to create an injection point for the auto-generated code produced by CommunityToolkit.Mvvm.
+// Dependencies > CommunityToolkit.Mvvm.SourceGenerators > CommunityToolkit.Mvvm.SourceGenerators.ObservablePropertyGenerator > WILLOWMAKER.ViewModels.MainViewModel.g.cs
+
+public partial class MainViewModel : ObservableObject
 {
-    public string GitHubLink => "https://github.com/Project-KONGOR-Open-Source";
-    public string WebPortalLink => "https://kongor.online/";
-    public string RedditLink => "https://www.reddit.com/r/Project_KONGOR/";
-    public string DiscordLink => "ლ(ಠ益ಠლ) But At What Cost?";
+    [ObservableProperty]
+    private string? _gitHubLink = "https://github.com/Project-KONGOR-Open-Source";
+
+    [ObservableProperty]
+    private string? _webPortalLink = "https://kongor.online/";
+
+    [ObservableProperty]
+    private string? _redditLink = "https://www.reddit.com/r/Project_KONGOR/";
+
+    [ObservableProperty]
+    private string? _discordLink = "ლ(ಠ益ಠლ) But At What Cost?";
+
+    [ObservableProperty]
+    private string? _masterServerAddress;
 }
--- WILLOWMAKER/ViewModels/ViewModelBase.cs
+++
@@ -1,7 +0,0 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-
-namespace WILLOWMAKER.ViewModels;
-
-public class ViewModelBase : ObservableObject
-{
-}
