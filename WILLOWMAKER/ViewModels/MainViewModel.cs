776deece36bf70fc1652b94d2c50abdfcadbb999
[K]ONGOR <project.kongor@proton.me>
2024-05-12 20:03:44 +0100

    Implement Show/Hide Custom Master Server Address Logic

--- WILLOWMAKER/ViewModels/MainViewModel.cs
+++ WILLOWMAKER/ViewModels/MainViewModel.cs
@@ -1,11 +1,16 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using Avalonia;
+using Avalonia.Controls;
+
+using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+
+using System.Collections.ObjectModel;
 using System.Diagnostics;
 
 namespace WILLOWMAKER.ViewModels;
 
 // This class needs to be partial, to create an injection point for the auto-generated code produced by CommunityToolkit.Mvvm.
-// Dependencies > CommunityToolkit.Mvvm.SourceGenerators > CommunityToolkit.Mvvm.SourceGenerators.ObservablePropertyGenerator > WILLOWMAKER.ViewModels.MainViewModel.g.cs
+// Dependencies > Analyzers > CommunityToolkit.Mvvm.SourceGenerators > CommunityToolkit.Mvvm.SourceGenerators.ObservablePropertyGenerator > WILLOWMAKER.ViewModels.MainViewModel.g.cs
 
 public partial class MainViewModel : ObservableObject
 {
@@ -22,9 +27,21 @@ public partial class MainViewModel : ObservableObject
     private string? _discordLink = "ლ(ಠ益ಠლ) But At What Cost?";
 
     [ObservableProperty]
-    private string? _masterServerAddress;
+    private ComboBoxItem? _masterServerAddress;
+
+    [ObservableProperty]
+    private string? _customMasterServerAddress;
+
+    [ObservableProperty]
+    private bool _canShowCustomMasterServerAddressField = false;
 
     [RelayCommand]
     private void GoToURL(string url)
         => Process.Start(new ProcessStartInfo() { FileName = url, UseShellExecute = true });
+
+    partial void OnMasterServerAddressChanged(ComboBoxItem? oldValue, ComboBoxItem? newValue)
+    {
+        if (newValue is not null)
+            CanShowCustomMasterServerAddressField = newValue.Content?.ToString()?.Contains("CUSTOM", StringComparison.OrdinalIgnoreCase) ?? false;
+    }
 }
