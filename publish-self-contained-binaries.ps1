f67d9478cf121d8cda35f7bcd528995f90ab8b5b
[K]ONGOR <project.kongor@proton.me>
2024-05-27 21:51:28 +0100

    Add PowerShell Script For Publishing Self-Contained Binaries

---
+++ publish-self-contained-binaries.ps1
@@ -0,0 +1,3 @@
+dotnet publish "WILLOWMAKER" -p:"PublishProfile=Windows.SingleFile.pubxml"
+dotnet publish "WILLOWMAKER" -p:"PublishProfile=Linux.SingleFile.pubxml"
+dotnet publish "WILLOWMAKER" -p:"PublishProfile=macOS.SingleFile.pubxml"
