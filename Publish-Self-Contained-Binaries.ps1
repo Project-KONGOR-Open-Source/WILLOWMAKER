5f78d9d7bcb0789984d577d777e0a6b4e2785e0f
[K]ONGOR <project.kongor@proton.me>
2026-04-18 14:35:30 +0100

    Switch To Native AOT Publishing

--- Publish-Self-Contained-Binaries.ps1
+++
@@ -1,3 +0,0 @@
-dotnet publish "WILLOWMAKER" -p:"PublishProfile=Windows.SingleFile.pubxml"
-dotnet publish "WILLOWMAKER" -p:"PublishProfile=Linux.SingleFile.pubxml"
-dotnet publish "WILLOWMAKER" -p:"PublishProfile=macOS.SingleFile.pubxml"
