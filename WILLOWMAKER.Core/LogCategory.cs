c64909b9ecf1b9077b9f488e91df4cbfba89821b
[K]ONGOR <project.kongor@proton.me>
2026-04-18 17:30:33 +0100

    Make Log Categories Same Size (10 Characters Long)

--- WILLOWMAKER.Core/LogCategory.cs
+++ WILLOWMAKER.Core/LogCategory.cs
@@ -5,10 +5,10 @@ namespace WILLOWMAKER.Core;
 /// </summary>
 public static class LogCategory
 {
-    public const string Command    = "COMMAND";
+    public const string Command    = "COMMAND___";
     public const string Executable = "EXECUTABLE";
     public const string Initialise = "INITIALISE";
     public const string Parameters = "PARAMETERS";
-    public const string Update     = "UPDATE";
-    public const string Version    = "VERSION";
+    public const string Update     = "UPDATE____";
+    public const string Version    = "VERSION___";
 }
